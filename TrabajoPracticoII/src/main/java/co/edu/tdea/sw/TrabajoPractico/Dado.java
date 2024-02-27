32e1997d706ab753384d74ac952a6f6877ce3654
randallstip <randallstip2003@hotmail.com>
2024-02-26 20:36:04 -0500

    add proyecto

---
+++ TrabajoPracticoII/src/main/java/co/edu/tdea/sw/TrabajoPractico/Dado.java
@@ -0,0 +1,11 @@
+package co.edu.tdea.sw.TrabajoPractico;
+
+import java.util.Scanner;
+
+public class Dado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+    }
+}
